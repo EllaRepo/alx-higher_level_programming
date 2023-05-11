31833f94ff358d20423c50da9ac80f13389eaff7
Eliab E <eliaberango@gmail.com>
2023-05-11 11:26:43 -0700

    Refactored the code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    nargs = len(sys.argv)
-    if nargs == 1:
-        print("0 arguments.")
-    else:
-        print("{} arguments:".format(nargs-1))
-        for item in sys.argv[1:]:
-            print("{}: {}".format(sys.argv.index(item), item))
+    nargs = len(sys.argv) - 1
+    msg_end = "s." if nargs == 0 else (":" if nargs == 1 else "s:")
+    print("{} argument{}".format(nargs, msg_end))
+    for item in sys.argv[1:]:
+        print("{}: {}".format(sys.argv.index(item), item))
