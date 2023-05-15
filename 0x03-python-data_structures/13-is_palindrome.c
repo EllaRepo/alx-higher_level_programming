95d42aad5dbbb12701abe922fd0ab6f86ed595cf
Eliab E <eliaberango@gmail.com>
2023-05-15 00:55:22 -0700

    Refactored the code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -33,7 +33,7 @@ int is_palindrome(listint_t **head)
 	listint_t *rev_list = NULL, *h, *rh;
 
 	if (*head == NULL || !(*head)->next ||
-	((*head)->n == (*head)->n && !(*head)->next->next))
+	((*head)->n == (*head)->next->n && !(*head)->next->next))
 		return (1);
 	for (h = *head; h; h = h->next)
 		add_nodeint(&rev_list, h->n);
