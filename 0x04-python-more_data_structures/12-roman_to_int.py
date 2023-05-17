a731ee4010dc88faf519da2cd9303384b61d7573
Eliab E <eliaberango@gmail.com>
2023-05-17 10:32:04 -0700

    refactored the code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,15 +4,12 @@ def roman_to_int(roman_string):
         return 0
     roman_nume = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                   'D': 500, 'M': 1000}
-    number = 0
-    prev = ''
+    number, prev, curr_val = 0, 0, 0
     for i in roman_string:
-        number += roman_nume[i]
-        if prev == 'I' and i in 'VX':
-            number -= 2
-        if prev == 'C' and i in 'DM':
-            number -= 200
-        if prev == 'X' and i in 'CL':
-            number -= 20
-        prev = i
+        curr_val = roman_nume[i]
+        if curr_val > prev:
+            number += curr_val - 2 * prev
+        else:
+            number += curr_val
+        prev = curr_val
     return number
