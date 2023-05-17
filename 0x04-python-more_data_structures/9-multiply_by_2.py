a020d1a21548314c5228e372d8db5d2ccf2266c9
Eliab E <eliaberango@gmail.com>
2023-05-17 09:11:39 -0700

    Refactored the code

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    keys = list(a_dictionary)
-    new_dic = dict()
-    for ids in keys:
-        new_dic[ids] = a_dictionary[ids] * 2
+    new_dic = {}
+    for key, value in a_dictionary.items():
+        new_dic[key] = value * 2
     return new_dic
