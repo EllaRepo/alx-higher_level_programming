9876e54c6634136b4a4768954a055d6f20cc3214
Eliab E <eliaberango@gmail.com>
2023-05-18 01:38:56 -0700

    Fix type error

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -8,9 +8,8 @@
 */
 void print_python_bytes(PyObject *p)
 {
-	PyBytesObject *bytes;
-	char *buffer;
-	Py_ssize_t size, i;
+	char *string, c;
+	long int size, i, limit;
 
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
@@ -19,21 +18,21 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	bytes = (PyBytesObject *)p;
-	size = ((PyVarObject *)p)->ob_size;
-	buffer = bytes->ob_sval;
+	size = ((PyVarObject *)(p))->ob_size;
+	string = ((PyBytesObject *)p)->ob_sval;
 
 	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", buffer);
+	printf("  trying string: %s\n", string);
 
-	size = = size > 10 ? 10 : size + 1;
+	limit = size >= 10 ? 10 : size + 1;
 
-	printf("  first %ld bytes:", size);
-	for (i = 0; i < size; i++)
-		if (buffer[i] >= 0)
-			printf(" %02x", buffer[i]);
-		else
-			printf(" %02x", buffer[i] + 0xFF4);
+	printf("  first %ld bytes:", limit);
+
+	for (i = 0; i < limit; i++)
+	{
+		c = string[i] >= 0 ? string[i] : string[i] + 256;
+		printf(" %02x", c);
+	}
 	printf("\n");
 }
 /**
@@ -44,11 +43,11 @@ void print_python_bytes(PyObject *p)
 */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size = 0, i = 0;
+	long int size, i;
 	PyObject *item;
-	const char *type_name;
 
 	size = ((PyVarObject *)(p))->ob_size;
+
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
@@ -56,9 +55,8 @@ void print_python_list(PyObject *p)
 	for (i = 0; i < size; i++)
 	{
 		item = ((PyListObject *)p)->ob_item[i];
-		type_name = item->ob_type->tp_name;
-		printf("Element %ld: %s\n", i, type_name);
-		if (PyBytes_Check(obj))
+		printf("Element %ld: %s\n", i, ((item)->ob_type)->tp_name);
+		if (PyBytes_Check(item))
 			print_python_bytes(item);
 	}
 }
