e0914e387e11a5dd8b9493f59bf18caeab995361
Eliab E <eliaberango@gmail.com>
2023-05-30 05:53:12 -0700

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square that defines a square.
+
+    """
+    def __init__(self, size):
+        """Initialize method that stores value for private attribute size.
+
+        Args:
+            size (int): Size of the square.
+
+        """
+        self.__size = size
