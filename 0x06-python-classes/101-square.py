06882a1743a8459e4688e225855339567f6a95d3
Eliab E <eliaberango@gmail.com>
2023-05-30 13:30:22 -0700

    Add 101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -5,7 +5,6 @@
 
 class Square:
     """Class Square that defines a square.
-
     """
     def __init__(self, size=0, position=(0, 0)):
         """Initialize method that stores value for private attribute size.
