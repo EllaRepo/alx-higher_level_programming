b71495cf5bb4120098c08414a9ebc2e0abb593c4
EllaRepo <eliaberango@gmail.com>
2023-07-18 17:08:13 -0400

    Fix error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,6 @@
 -- Sript that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server
 -- Convert the hbtn_0c_0 database to UTF8 with the specified collation
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+USE hbtn_0c_0;
 -- Convert the first_table table to UTF8 with the specified collation
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Modify the name field to use the UTF8 character set and collation
-ALTER TABLE first_table MODIFY name VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
