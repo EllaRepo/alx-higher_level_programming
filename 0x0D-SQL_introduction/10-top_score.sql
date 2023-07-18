69e674e2e9d39deec9e7e62f8a468de112da0616
EllaRepo <eliaberango@gmail.com>
2023-07-18 16:44:12 -0400

    Add 10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Script lists all records of the second_table table
+-- Select the score and name columns from the second_table table, ordered by score
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
