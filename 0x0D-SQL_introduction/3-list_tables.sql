0511a576d93e5af719f71c0de7e1be42c7377182
EllaRepo <eliaberango@gmail.com>
2023-07-18 16:23:57 -0400

    fix error

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,9 +1,3 @@
--- Script that lists all tables in a database
-
--- Set the database name as a variable
-SET @dbName = 'mysql';
-
+-- Script that lists all tables in the specified database
 -- Show the names of all tables in the database
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = @dbName;
+SHOW TABLES;
