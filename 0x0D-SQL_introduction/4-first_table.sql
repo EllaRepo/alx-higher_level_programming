2949621e4c67612820167dc385ad61b9585515f0
EllaRepo <eliaberango@gmail.com>
2023-07-18 16:26:26 -0400

    Add 4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Script creates the first_table table if it does not already exist
+-- Create the table if it does not already exist
+CREATE TABLE IF NOT EXISTS first_table (
+  id INT,
+  name VARCHAR(256));
