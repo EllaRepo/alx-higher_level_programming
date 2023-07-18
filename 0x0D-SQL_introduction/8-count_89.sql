2c1a40dc611308fd340d67790a9c5156b688dd61
EllaRepo <eliaberango@gmail.com>
2023-07-18 16:37:57 -0400

    Add 8-count_89.sql

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Script that displays the number of records with id = 89 in the first_table table
+-- Count the number of rows with id = 89 in the first_table table
+SELECT COUNT(*) as id FROM first_table WHERE id = 89;
