3a350fb0a57b3c9a8162604e3c1534b11c8454c1
EllaRepo <eliaberango@gmail.com>
2023-07-18 16:52:38 -0400

    Add 12-no_cheating.sql

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- Script that updates the score of Bob to 10 in the second_table table
+-- Update the score of the record with name = "Bob" to 10
+UPDATE second_table
+SET score = 10
+WHERE name = "Bob";
