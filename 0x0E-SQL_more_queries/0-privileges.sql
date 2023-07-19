e5463551c78c90dce8bd4a6770c88e11107f8b01
EllaRepo <eliaberango@gmail.com>
2023-07-19 14:53:12 -0400

    Add semi colon at the end

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
---script that lists all privileges of the MySQL users 
---List all privileges of the MySQL users user_0d_1 and user_0d_2
+--script that lists all privileges of the MySQL users
+--Query to list all privileges of the MySQL users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
