d637c6bbdf03c47973056b0a4ea4ffd085cf2aa8
EllaRepo <eliaberango@gmail.com>
2023-07-19 15:00:59 -0400

    Refactored the code

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,4 @@
 --script that lists all privileges of the MySQL users
 --Query to list all privileges of the MySQL users
-SELECT *
-FROM mysql.user
-WHERE User IN ('user_0d_1', 'user_0d_2')
-    AND Host = 'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
