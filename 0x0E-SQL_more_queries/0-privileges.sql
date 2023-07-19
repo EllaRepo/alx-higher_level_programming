46fac7f9730826dcef04f7a44101418945f357ec
EllaRepo <eliaberango@gmail.com>
2023-07-19 16:53:12 -0400

    Refactored

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- Script that lists all privileges of the MySQL users
--- List all privileges of MySQL user user_0d_1
+-- List all privileges (GRANT) of MySQL user
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- List all privileges of MySQL user user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
