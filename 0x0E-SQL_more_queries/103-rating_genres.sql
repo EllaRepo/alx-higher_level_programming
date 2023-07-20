ad308681e56b28cdda67d08679ebe769afefe2f2
EllaRepo <eliaberango@gmail.com>
2023-07-20 12:45:36 -0400

    Add 103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,6 @@
 -- Script that lists all genres in the database hbtn_0d_tvshows_rate by their rating
 -- lists all genres in the database hbtn_0d_tvshows_rate by their rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
