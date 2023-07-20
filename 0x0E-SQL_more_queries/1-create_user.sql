568c82bd2363328fb4eec1ec34d3b66a4961c667
EllaRepo <eliaberango@gmail.com>
2023-07-20 05:27:44 -0400

    Refactored

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Script that creates an user in MySQL server
 -- create the user 'user_0d_1' in MySQL server
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-SET PASSWORD FOR 'user_0d_1' = 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
