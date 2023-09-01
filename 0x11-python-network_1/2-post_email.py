0ec887a0bb6004aaf9960034760088065fa11db9
EllaRepo <eliaberango@gmail.com>
2023-09-01 15:45:24 -0400

    Fix error

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,15 +2,15 @@
 """ sends a POST request to the passed URL with the email as a parameter,
     and displays the body of the response (decoded in utf-8)
 """
-from urllib import parse, request
+from urllib import request, parse
 import sys
 
 
 if __name__ == "__main__":
     values = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(values)
+    data = parse.urlencode(values)
     data = data.encode('ascii')
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as response:
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
         body = response.read()
         print(body.decode('utf-8'))
