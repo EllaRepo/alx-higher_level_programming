4add67d9d06dc98e5851e27ea375a6a186da786c
EllaRepo <eliaberango@gmail.com>
2023-09-01 15:34:30 -0400

    Add 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,8 +2,7 @@
 """ sends a POST request to the passed URL with the email as a parameter,
     and displays the body of the response (decoded in utf-8)
 """
-import urllib.parse
-import urllib.request
+from urllib import parse, request
 import sys
 
 
