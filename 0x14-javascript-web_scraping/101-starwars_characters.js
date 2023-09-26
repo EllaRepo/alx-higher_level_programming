63ca0dd5688260db364a04697cbeaf051916419b
Eliab E <eliaberango@gmail.com>
2023-09-26 15:21:05 -0700

    Fix Error

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,29 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/';
-let movieId = parseInt(process.argv[2], 10);
-let characters = [];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, (error, response, body) => {
   if (error == null) {
-    if (movieId < 4) {
-      movieId += 3;
-    } else if (movieId < 7) {
-      movieId -= 3;
-    }
-    const results = JSON.parse(body).results;
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === movieId) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (error, response, body) {
-        if (error == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    const characters = JSON.parse(body);
+    printCharacterNames(characters, 0);
   }
 });
+
+function printCharacterNames (characters, i) {
+  request(characters[i], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (i + 1 < characters.length) {
+        printCharacterNames(characters, i + 1);
+      }
+    }
+  });
+}
+
+printCharacterNames(process.argv[2]);
