1f8441ae3f160ca3ebc826591b78d7f32b791950
Eliab E <eliaberango@gmail.com>
2023-09-26 13:12:39 -0700

    Handle error in getting url status

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,8 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url).on('response', function (response) {
-  console.log(response.statusCode);
+request.get(url).on('response', function (error, response) {
+  if (error == null) {
+    console.log('code: ' + response.statusCode);
+  }
 });
