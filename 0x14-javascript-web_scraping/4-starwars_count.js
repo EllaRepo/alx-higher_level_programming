5829703cd42c7f5f27b56d37b7fd9d20f6e84098
Eliab E <eliaberango@gmail.com>
2023-09-26 13:51:21 -0700

    Fix error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
+let count = 0;
 
 request(url, (error, response, body) => {
   if (error == null) {
     const films = JSON.parse(body).results;
-    const wedgeAntillesFilms = films.filter((film) => {
-      const characters = film.characters;
-      return characters.search('18');
-    });
-    console.log(wedgeAntillesFilms.length);
+    for (let i = 0; i < films.length; i++) {
+      const characters = films[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].search('18') > 0) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
 });
