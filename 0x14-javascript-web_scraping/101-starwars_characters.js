0c39cef7a4aaafe98e0a5cbce2dc9e8c45ae88a7
Eliab E <eliaberango@gmail.com>
2023-09-26 15:33:25 -0700

    Fix Error

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,19 +5,17 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
   if (error == null) {
     const characters = JSON.parse(body);
-    printCharacterNames(characters, 0);
+    printCharacterNamesSync(characters, 0);
   }
 });
 
-function printCharacterNames (characters, i) {
+function printCharacterNamesSync (characters, i) {
   request(characters[i], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
       if (i + 1 < characters.length) {
-        printCharacterNames(characters, i + 1);
+        printCharacterNamesSync(characters, i + 1);
       }
     }
   });
 }
-
-printCharacterNames(process.argv[2]);
