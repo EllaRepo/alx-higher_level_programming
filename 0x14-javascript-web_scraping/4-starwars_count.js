eb01a50f061c7fc2fc3b3d9934c054b89afe9b20
Eliab E <eliaberango@gmail.com>
2023-09-26 13:38:44 -0700

    Add 4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error == null) {
+    const films = JSON.parse(body).results;
+    const wedgeAntillesFilms = films.filter((film) => {
+      const characters = film.characters;
+      return characters.includes('https://swapi-api.alx-tools.com/api/people/18/');
+    });
+    console.log(wedgeAntillesFilms);
+  }
+});
