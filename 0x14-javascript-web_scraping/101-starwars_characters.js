7e2a63a7523e66022b7dc37635931b1edc1da113
Eliab E <eliaberango@gmail.com>
2023-09-27 05:21:03 -0700

    Refactored

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,29 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/';
-let movieId = parseInt(process.argv[2], 10);
-let characters = [];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const results = JSON.parse(body).results;
-    if (movieId < 4) {
-      movieId += 3;
-    } else {
-      movieId -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === movieId) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharactersName(characters, 0);
   }
 });
+
+function printCharactersName (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (error === null) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharactersName(characters, index + 1);
+      }
+    }
+  });
+}
